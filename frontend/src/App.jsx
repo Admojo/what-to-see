034373b7187aa92eb13a78188e20f073391fe201
fredrikkri <fjkri@hotmail.com>
2024-04-26 12:41:23 +0200

    laget enkel innlogginside, som bare inneholder tekst for øyeblikket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Layout from './components/Layout'
+import LoginPage from './components/LoginPage'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    </>
+    <Layout>
+      <Routes>
+            <Route path="/" element={<LoginPage />}/>
+        </Routes>
+    </Layout>
   )
 }
 
-export default App
+export default App 
