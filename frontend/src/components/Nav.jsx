2ce2acdd8f9031556a32fd6002453ffd4c71c0c2
fredrikkri <fjkri@hotmail.com>
2024-04-26 14:05:52 +0200

    fikset på nav-bar. brukt sass og gjort det pent

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Nav() {
 
     return(
     <nav>
+        <Link href="#" id="logoButton">What to see</Link>
         <ul>
-            <li>What to see</li>
-            <li>Hva skal jeg se?</li>
-            <li>Bla gjennom sjangere</li>
-            <li>USERNAME</li>
+            <li id="pinkButtonNav"><button> Hva skal jeg se?</button></li>
+            <li><Link>Bla gjennom sjangere</Link></li>
+            <li><Link>USERNAME</Link></li>
         </ul>
     </nav>
     )
