2bdaa86c0c4ac9512ce2f8ada85705f6aa2b3379
fredrikkri <fjkri@hotmail.com>
2024-04-26 12:49:00 +0200

    begynnelsen på nav-bar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,3 +1,13 @@
 export default function Nav() {
 
+    return(
+    <nav>
+        <ul>
+            <li>What to see</li>
+            <li>Hva skal jeg se?</li>
+            <li>Bla gjennom sjangere</li>
+            <li>USERNAME</li>
+        </ul>
+    </nav>
+    )
 }
