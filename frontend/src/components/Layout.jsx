254c83e80eb34276fb7736b15aa628a94c84bf8b
fredrikkri <fjkri@hotmail.com>
2024-05-07 09:36:00 +0200

    litt css endringer, og lagt til en if-check der jeg returnerer nav-bar hvis man er logget inn. men hvis ikke, så blir den ikke returnert

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,15 +1,25 @@
 import Nav from "./Nav"
 
-export default function Layout({children}){
-    return(
-    <div id="container">
-        <header>
-            <Nav></Nav>
-        </header>
-        <main>
-            {children}
-        </main>
-        <footer> Copyright Gruppe 4 - 2024 </footer>
-    </div>
-    )
+export default function Layout({children, username}){
+
+    if (username != null) {
+        return(
+        <div id="container">
+            <header>
+                <Nav></Nav>
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer> Copyright Gruppe 4 - 2024 </footer>
+        </div>
+        )
+    }
+    else {
+        return(
+            <main>
+                {children}
+            </main>
+        )
+    }
 }
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,7 +3,6 @@ import { FaCircleUser } from "react-icons/fa6";
 import { FaTv } from "react-icons/fa";
 
 export default function Nav() {
-
     return(
     <nav>
         <Link href="#" id="logoButton">What to see?</Link>
