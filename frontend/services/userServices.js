9dbf552cb92c7c080fb61e0e1c6fa66020447146
evjengit <ole_fram.96@hotmail.com>
2024-05-07 11:24:28 +0200

    sorterer fetchallusers alfabetisk

--- frontend/services/userServices.js
+++ frontend/services/userServices.js
@@ -2,7 +2,7 @@ import { client } from "../sanity/client";
 
 export async function fetchAllUsers(){
 
-    const data = await client.fetch(`*[_type == "users"]{
+    const data = await client.fetch(`*[_type == "users"] | order(name asc){
         _id,
         _type,
         name
