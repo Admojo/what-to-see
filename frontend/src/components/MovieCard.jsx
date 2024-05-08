a24bf6bb393af78889f639e0caf02b8ea48adca6
evjengit <ole_fram.96@hotmail.com>
2024-05-08 13:25:03 +0200

    ordnet så moviecard kan ta imot data fra API
    Fikset så genrepage sender med data

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,8 +1,10 @@
-export default function MovieCard({imdb}) {
+import { Link } from "react-router-dom";
+
+export default function MovieCard({imdb, title, image}) {
     return (
     <article className="movieCard">
-        <img src="#" alt="PLACEHOLDER" />
-        <h3>FILMNAVN</h3>
+        <img src={image === null ? "#" : image} alt="Movie Cover" />
+        <h3><Link to={`https://m.imdb.com/title/${imdb}`}>{title}</Link></h3>
     </article>
     )
 }
