f7be90defbfa4644c9607e7d3cbfacf9516135f1
fredrikkri <fjkri@hotmail.com>
2024-05-16 21:15:32 +0200

    Revert "henta fra fredrik_b"
    This reverts commit 88cee8acaf38a17a6ecde4e11007441d005b1868.

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,33 +1,17 @@
-import { Link, redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaCircleUser } from "react-icons/fa6";
 import { FaTv } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom'
 
 export default function Nav({user}) {
 
-    const currentUser = localStorage.getItem("username");
-    const redirectToLoginPage = useNavigate();
-
-    const handleLogout = () => {
-        localStorage.setItem("username", "");
-        redirectToLoginPage("/login")
-        // for å kunne være på loginpage uten at nav-bar er synlig så må man refreshe siden først. Dette løste vi med en quickfix med linjen under.
-        // fant løsningen på stackowerflow: https://stackoverflow.com/questions/41481522/how-to-refresh-a-page-using-react-route-link
-        window.location.reload();
-    }
-    if (user !== "") {
     return(
     <nav>
         <Link to="/home" id="logoButton">What to see?</Link>
         <ul id="menyPunkter">
             <li id="pinkButtonNav">< Link to="/home"><button><FaTv /> Hva skal jeg se?</button></Link></li>
             <li><Link to ="/genre">Bla gjennom sjangere</Link></li>
-            <li><FaCircleUser /> {currentUser}</li>
-            <li><button id="logoutButton" onClick={handleLogout}>Logout</button></li>
+            <li><FaCircleUser /> {user.name}</li>
         </ul>
     </nav>
     )
-} else {
-    return(<></>)
-}
 }
