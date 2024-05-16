a9d9283946e7e8871e91101078f0d7ce71832a38
Andrea <andrea.s.nilsson@hiof.no>
2024-05-16 14:58:29 +0200

    fremgang så langt, wishListUser skriver ut til console, men må få skrevet til komponent

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -6,25 +6,29 @@ import { useEffect, useState } from "react";
 
 export default function HomePage({user, movielist, userList/*, title*/}){
 
-    const [wishList, setWishList] = useState (null)
+    const [wishList, setWishList] = useState ([])
 
     //Skriver ut listen over brukerer som ikke er innlogget
     const otherUsers = userList.filter(friends => friends !== user)
 
     //Henter innhold i en ønskeliste til en bruker
-    const wishListUser =  async (user) => {
-        const data = await fetchWishListForUser(user.name)
+    const wishListUser =  async (username) => {
+        const data = await fetchWishListForUser(username)
         setWishList (data)
-        console.log("wishListUser:", data, "username:", user.name)
+        console.log("wishListUser:", data, "username:", username)
     } 
     
     useEffect (() => {
-        wishListUser(user)
+        wishListUser(user.name)
     }, [user])
 
-    // console.log("wishList1:", wishList)
+    console.log("wishList1:", wishList)
+    console.log("wishList2:", user.wishlist)
 
+    // const wl = user.wishlist
 
+    // const output = wishListUser.forEach((title) => console.log (title))
+    // const onskelist = wishList
  
     return (
         <>
@@ -32,11 +36,21 @@ export default function HomePage({user, movielist, userList/*, title*/}){
             <div>
                 <section id="moviesWatchLaterSection">
                     <h2><FaStar /> Filmer jeg skal se!</h2>
-                    {/* {wishList?.map((movie, index) => (
-                        <div key={index}>
-                            <p>{movie}</p> 
-                        </div>
-                        ))} */}
+                    {wishList?.map((movie, index) => <MovieCard key={index} titleSanity={movie}/>)}
+                    {/* {wl?.map((movie, index) => <MovieCard key={index} titleSanity={movie}/>)} */}
+                    {wishList.length > 0 ? (wishList.map((movie, index) => (
+                        <MovieCard key={index} titleSanity={movie}/>
+                        ))
+                        ) : (
+                            <p> Ingen filmer i ønskeliste </p>
+                        )}
+
+                    {/* {wl.length > 0 ? (wl?.map((movie, index) => (
+                        <MovieCard key={index} titleSanity={movie}/>
+                        ))
+                        ): (
+                            <p> Ingen filmer i ønskeliste </p>
+                        )}   */}
                     <ul>
                         <li>
                             <MovieCard />
--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ export default function MovieCard({imdb, title, image, titleSanity}) {
     return (
     <article className="movieCard">
         <img src={image === null ? "#" : image} alt="Movie Cover" />
-        <h3>{titleSanity}</h3>
+        {/* <h3>{titleSanity}</h3> */}
         <h3><Link to={`https://m.imdb.com/title/${imdb}`}>{title}</Link></h3>
     </article>
     )
