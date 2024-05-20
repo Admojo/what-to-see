fdc02d00a09b34021fabdcecd43b14079ce3d4e8
fredrikkri <fjkri@hotmail.com>
2024-05-20 14:12:35 +0200

    fjernet kommentar

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,8 +3,6 @@ import Nav from "./Nav"
 export default function Layout({children}){
 
     const currentUsername = localStorage.getItem("username")
-    // fikk ikke til at logikken under fungerer. Navbar er synlig selv om bruker ikke er logget inn. 
-    // Hvis knappene trykkes på når bruker ikke er logget inn, så vil dette føre til errors.
         return(
         <> {currentUsername ? (
             <header>
