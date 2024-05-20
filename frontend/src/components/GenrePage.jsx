c995fe40f9669deba709841f15f7674a391a4473
evjengit <ole_fram.96@hotmail.com>
2024-05-20 19:50:17 +0200

    Ordnet neste og forrige side knapper som viser de neste filmene på genrepage

--- frontend/src/components/GenrePage.jsx
+++ frontend/src/components/GenrePage.jsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import { options } from "../App";
 
 export default function GenrePage({genre, movielist, setMovies}){
   
+    const [page, setPage] = useState(1)
     const localgenre = localStorage.getItem("genre", genre)
-    const url = `https://moviesdatabase.p.rapidapi.com/titles?info=base_info&genre=${localgenre}`;
+    const url = `https://moviesdatabase.p.rapidapi.com/titles?info=base_info&genre=${localgenre}&page=${page}&limit=10`;
 
     const getData = async(url) => {
         try {
@@ -17,15 +18,31 @@ export default function GenrePage({genre, movielist, setMovies}){
         }
       }
 
+    const handleNextClick = () => {
+      setPage(prevPage => prevPage + 1)
+    }
+
+    const handlePrevClick = () => {
+    setPage(prevPage => Math.max(1, prevPage - 1));
+    }
+
     useEffect(() => {
         getData(url)
-      },[])
+      },[page])
     
     return (
+      <>
         <section id="singleGenreContent">
         {movielist?.results?.map((item, i) => 
             <MovieCard key={i+"ok"} imdb={item.id} title={item.titleText.text} image={item.primaryImage?.url}/>
         )}
         </section>
+        <nav>
+          <ul id="pageselector">
+            <button onClick={handlePrevClick}>Forrige Side</button>
+            <button onClick={handleNextClick}>Neste Side</button>
+          </ul>
+        </nav>
+      </>
     ) 
 }
